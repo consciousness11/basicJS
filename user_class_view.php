3b307ea56d1ed25bc108613d643b3b04c12a239f
Mark <marut.dhungana@gmail.com>
2022-04-15 14:43:10 +0545

    New code added

--- user_class_view.php
+++ user_class_view.php
@@ -1,8 +1,29 @@
 <?php 
+include_once 'user_class_model.php';
 class UserView {
-    public function display($data){
-        echo $data;
+    public function display($models) {
+        var_dump($models);
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<th>Username</th>";
+        echo "<th>Password</th>";
+        echo "<th>Action</th>";
+        echo "</tr>";
+        foreach ($models as $model) {
+            if ($model->getId() != NULL) 
+            {
+            echo "<tr>";
+            echo "<td>" . $model->getId() . "</td>";
+            echo "<td>" . $model->getUsernames() . "</td>";
+            echo "<td>" . $model->getPasswords() . "</td>";
+            echo "<td>" . "Add/Edit" . "</td>";
+            echo "</tr>";
+            }
+        }
+        echo "</table>";
     }
+    
 }
 
 ?>
